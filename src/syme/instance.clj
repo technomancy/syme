e4db2e34b6fbd600b27da28a44f07f32bba7b7b6
Hugo Duncan <hugo@hugoduncan.org>
2013-01-10 18:25:42 -0500

    Use p.compute/compute-service and add destroy

--- src/syme/instance.clj
+++ src/syme/instance.clj
@@ -26,29 +26,44 @@
                  (:body) (.split "\n"))]
     (map (memfn getBytes) keys)))
 
-(defn launch [username {:keys [project invite identity credential]}]
+
+(defn group [username {:keys [project invite identity credential]}]
+  (pallet/group-spec
+   (str username "/" project)
+   :count 1
+   :node-spec (pallet/node-spec
+               :image {:os-family :ubuntu
+                       :image-id "us-east-1/ami-3c994355"})
+   :phases {:bootstrap (api/plan-fn (admin/automated-admin-user
+                                     "syme"))
+            :configure (api/plan-fn
+                        (doseq [u (cons username (.split invite ",? +"))]
+                          (apply admin/automated-admin-user username
+                                 (get-keys username)))
+                        ;; TODO: need to do these as primary user
+                        (actions/exec-script
+                         (format "git clone git://github.com/%s/%s.git"
+                                 username project)))}))
+
+(defn launch [username {:keys [project invite identity credential] :as options}]
   (force write-key-pair)
   (pallet/converge
-   (pallet/group-spec
-    (str username "/" project)
-    :count 1
-    :node-spec (pallet/node-spec
-                :image {:os-family :ubuntu
-                        :image-id "us-east-1/ami-3c994355"})
-    :phases {:bootstrap #(admin/automated-admin-user
-                          "syme" (.getBytes (:public-key env)))
-             :configure (fn []
-                          (doseq [u (cons username (.split invite ",? +"))]
-                            (apply admin/automated-admin-user username
-                                   (get-keys username)))
-                          ;; TODO: need to do these as primary user
-                          (actions/exec-script
-                           (format "git clone git://github.com/%s/%s.git"
-                                   username project)))})
+   (group username options)
    ;; apparently :user is broken in 0.8.0-alpha
    :user (api/make-user "syme"
                         :public-key-path pubkey
                         :private-key-path privkey)
-   :compute (config/compute-service "aws-ec2"
+   :compute (pallet.compute/compute-service "aws-ec2"
+                                    :identity identity
+                                    :credential credential)))
+
+(defn destroy
+  [username {:keys [project invite identity credential] :as options}]
+  (pallet/converge
+   {(group username options) 0}
+   :user (api/make-user "syme"
+                        :public-key-path pubkey
+                        :private-key-path privkey)
+   :compute (pallet.compute/compute-service "aws-ec2"
                                     :identity identity
                                     :credential credential)))
