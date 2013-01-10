70be775ef92c525fa17eb0b82df7c3f9540e4882
Phil Hagelberg <technomancy@gmail.com>
2013-01-10 15:21:51 -0800

    Handle packages.

--- src/syme/instance.clj
+++ src/syme/instance.clj
@@ -2,7 +2,7 @@
   (:require [pallet.core :as pallet]
             [pallet.api :as api]
             [pallet.actions :as actions]
-            [pallet.configure :as config]
+            [pallet.compute :as compute]
             [pallet.crate.automated-admin-user :as admin]
             [pallet.phase :as phase]
             [clj-http.client :as http]
@@ -15,6 +15,10 @@
 (def privkey (str (io/file (System/getProperty "user.dir")
                            "data" "keys" "syme")))
 
+(def user (api/make-user "syme"
+                         :public-key-path pubkey
+                         :private-key-path privkey))
+
 (def write-key-pair
   (delay
    (.mkdirs (io/file "data" "keys"))
@@ -26,30 +30,29 @@
                  (:body) (.split "\n"))]
     (map (memfn getBytes) keys)))
 
-(defn launch [username {:keys [project invite identity credential compute]}]
+(defn launch [username {:keys [project invite identity credential
+                               packages compute] :as opts}]
   (force write-key-pair)
   ;; NFI
-  (alter-var-root #'pallet.core.user/*admin-user*
-                  (constantly (api/make-user "syme"
-                                             :public-key-path pubkey
-                                             :private-key-path privkey)))
+  (alter-var-root #'pallet.core.user/*admin-user* (constantly user))
   (pallet/converge
    (pallet/group-spec
     (str username "/" project)
     :count 1
-    :node-spec (pallet/node-spec
-                :image {:os-family :ubuntu
-                        :image-id "us-east-1/ami-3c994355"})
+    :node-spec (pallet/node-spec :image {:os-family :ubuntu
+                                         :image-id "us-east-1/ami-3c994355"})
     :phases {:bootstrap #(admin/automated-admin-user
                           "syme" (.getBytes (:public-key env)))
              :configure (fn []
                           (doseq [u (cons username (.split invite ",? +"))]
-                            (apply admin/automated-admin-user username
-                                   (get-keys username)))
-                          ;; TODO: need to do these as primary user
-                          (actions/exec-script
-                           (format "git clone git://github.com/%s/%s.git"
-                                   username project)))})
-   :compute (config/compute-service (or compute "aws-ec2")
-                                    :identity identity
-                                    :credential credential)))
+                            (println "Adding admin" u)
+                            (apply admin/automated-admin-user u (get-keys u)))
+                          (doseq [p (cons "git" (.split packages ",? +"))]
+                            (actions/package p))
+                          (actions/exec-checked-script
+                           "Initial clone"
+                           (format "sudo -iu %s git clone git://github.com/%s/%s.git"
+                                   username username project)))})
+   :compute (compute/compute-service (or compute "aws-ec2")
+                                     :identity identity
+                                     :credential credential)))
