ed03378074f9925ce9e148da035d9793973a31bb
Phil Hagelberg <technomancy@gmail.com>
2013-05-02 19:20:11 -0700

    Typo in language script.

--- resources/languages/Clojure.sh
+++ resources/languages/Clojure.sh
@@ -1,3 +1,3 @@
 sudo apt-get install -y openjdk-7-jre-headless
 sudo wget -qO /usr/local/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein
-sudo chmod +x /urs/local/bin/lein
+sudo chmod +x /usr/local/bin/lein
