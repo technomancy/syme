4a09e92d41d55cb3946a3242fd3352b4c57bc2ea
Phil Hagelberg <technomancy@gmail.com>
2013-10-25 18:54:53 -0700

    Add Erlang to language support dir.

---
+++ resources/languages/Erlang.sh
@@ -0,0 +1,12 @@
+wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+wget http://packages.erlang-solutions.com/debian/erlang_solutions.asc
+
+sudo dpkg -i erlang-solutions_1.0_all.deb && rm erlang-solutions_1.0_all.deb
+sudo apt-key add erlang_solutions.asc && rm erlang_solutions.asc
+
+sudo apt-get update
+sudo apt-get install -y erlang-nox
+
+wget https://github.com/rebar/rebar/wiki/rebar
+chmod +x rebar
+sudo mv rebar /usr/local/bin
